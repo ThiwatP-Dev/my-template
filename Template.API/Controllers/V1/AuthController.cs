9b230f8873d57ea8742934d547f2292b5effd66a
thiwat.p <thiwat@greenmoons.co.th>
2024-09-27 09:12:53 +0700

    add logged

--- Template.API/Controllers/V1/AuthController.cs
+++ Template.API/Controllers/V1/AuthController.cs
@@ -9,16 +9,22 @@ namespace Template.API.Controllers.V1;
 [ApiController]
 [Route(BaseUrl)]
 public class AuthController(IAuthService authService,
-                            IUserService userService) : BaseController
+                            IUserService userService,
+                            ILoggerFactory loggerFactory) : BaseController
 {
     private readonly IAuthService _authService = authService;
     private readonly IUserService _userService = userService;
+    private readonly ILogger _logger = loggerFactory.CreateLogger<AuthController>();
 
     [AllowAnonymous]
     [HttpPost("login")]
     public async Task<IActionResult> LoginAsync([FromBody] AccessTokenRequestDto request)
     {
+        _logger.LogInformation("Login with username: {username}.", request.Username);
+
         var response = await _authService.LoginAsync(request.Username, request.Password);
+
+        _logger.LogInformation("Login succeeded.");
 
         var userId = GetCurrentUser();
         
@@ -30,12 +36,18 @@ public class AuthController(IAuthService authService,
     [HttpPost("login/google")]
     public async Task<IActionResult> LoginGoogleAsync()
     {
+        _logger.LogInformation("Login with google.");
+
         if (!Request.Headers.TryGetValue("Authorization", out var authHeaderValue))
         {
+            _logger.LogError("Unauthorize.");
+
             return Unauthorized();
         }
 
         var response = await _authService.LoginGoogleAsync(authHeaderValue!);
+
+        _logger.LogInformation("Login succeeded.");
 
         var userId = GetCurrentUser();
         
@@ -47,7 +59,11 @@ public class AuthController(IAuthService authService,
     [HttpPost("generate-super-admin")]
     public async Task <IActionResult> GenerateSuperAdmin()
     {
+        _logger.LogInformation("Generate super admin.");
+
         await _userService.GenerateSuperAdmin();
+
+        _logger.LogInformation("Super admin generated.");
         var location = Url.Action(nameof(GenerateSuperAdmin));
         return Created(location, null);
     }
