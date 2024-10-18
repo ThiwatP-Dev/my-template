268d5f4608b3ddb8040c92416617d2e50aed3266
thiwatp <thiwat@greenmoons.co.th>
2024-10-18 08:18:50 +0700

    update test

--- Template.Test/InstituteTest.cs
+++ Template.Test/InstituteTest.cs
@@ -1,33 +1,15 @@
-using Microsoft.EntityFrameworkCore;
-using Template.Database;
-using Template.Core.UnitOfWorks.Interfaces;
-using Template.Core.UnitOfWorks;
 using Template.Service.Interfaces;
 using Template.Service.src;
 using Template.Service.Dto;
-using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace Template.Test;
 
-public class InstituteTest
+public class InstituteTest : BaseTest
 {
-    private readonly DatabaseContext _dbContext;
-    private readonly IUnitOfWork _unitOfWork;
     private readonly IInstituteService _service;
 
     public InstituteTest()
     {
-        var options = new DbContextOptionsBuilder<DatabaseContext>()
-            .UseInMemoryDatabase(databaseName: "TestDatabase")
-            .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning))
-            .Options;
-
-        _dbContext = new DatabaseContext(options);
-        _dbContext.Database.BeginTransaction(); 
-        _dbContext.Database.EnsureDeleted();
-        _dbContext.Database.EnsureCreated();
-
-        _unitOfWork = new UnitOfWork(_dbContext);
         _service = new InstituteService(_unitOfWork);
     }
 
